04eec5271c2b98fc5a871718f911cd88bd4b62e6
adrianml14 <adrianml14@educastur.es>
2025-03-09 18:26:25 +0100

    update.user

--- Frontend/frontend/src/app/components/update-user/update-user.component.ts
+++ Frontend/frontend/src/app/components/update-user/update-user.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { UsersService } from '../../users.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-update-user',
@@ -7,5 +11,35 @@ import { Component } from '@angular/core';
   styleUrl: './update-user.component.css'
 })
 export class UpdateUserComponent {
+  user: any 
+  data: any
+
+  constructor(private userService: UsersService, private route: ActivatedRoute, private router: Router){}
+
+  ngOnInit(): void{
+    let id = this.route.snapshot.params['id']
+    this.userService.getUserById(id).subscribe(data=> {
+      this.user = data
+      console.log(data)
+    })
+  }
+
+  form = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required)
+  })
+
+  submit(){
+    this.data = this.form.value
+    this.user.name = this.data.name
+    this.user.email = this.data.email
+    console.log(this.data)
+
+    this.userService.updateUser(this.user?.id, this.user).subscribe(data=> {
+      console.log(data)
+    })
+
+    this.router.navigate(['/'])
+  }
 
 }
