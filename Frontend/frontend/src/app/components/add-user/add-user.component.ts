88fadb1b1369147d1df82f325010ced0e3482877
adrianml14 <adrianml14@educastur.es>
2025-03-09 18:07:29 +0100

    add-User

--- Frontend/frontend/src/app/components/add-user/add-user.component.ts
+++ Frontend/frontend/src/app/components/add-user/add-user.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsersService } from '../../users.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-user',
@@ -7,5 +11,21 @@ import { Component } from '@angular/core';
   styleUrl: './add-user.component.css'
 })
 export class AddUserComponent {
+  constructor(private  userService: UsersService, private router: Router){}
+
+  data: any
+
+  form = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    gender: new FormControl('', Validators.required)
+  })
+
+  addUser(){
+    this.data = this.form.value
+    this.userService.addUser(this.data).subscribe(data=>{
+      this.router.navigate(['/']) //redireccionar al home
+    })
+  }
 
 }
