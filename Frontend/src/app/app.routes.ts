f35156a03599ccaed1ad9520ffde9f4f60b6db90
adrianml14 <adrianml14@educastur.es>
2025-03-09 20:12:30 +0100

    rutas

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ViewUsersComponent } from './components/view-users/view-users.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
+import { UpdateUserComponent } from './components/update-user/update-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: ViewUsersComponent},
+    {path: 'add', component: AddUserComponent},
+    {path: 'update/:id', component: UpdateUserComponent},
+];
