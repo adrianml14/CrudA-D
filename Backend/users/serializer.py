46f31a083b9e91c53b324f038bc1b6310394c0eb
adrianml14 <adrianml14@educastur.es>
2025-03-09 19:53:40 +0100

    adios backend

--- Backend/users/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User # El modelo que se va a serializar
-        fields = ('id', 'name', 'email', 'gender')
--- Backend/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django import urls
-from .views import UserDetail, UserList, helloWorld
-
-urlpatterns = [
-    path('users/', UserList.as_view(), name='Users'),
-    path('user/<int:pk>', UserDetail.as_view(), name = 'User'),
-    path('hello/', helloWorld, name = 'hello'),
-]
